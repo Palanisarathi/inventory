e53d011f9730030f21d3efc213e24b8868c703cd
Palani P <palaniparthasarathi@gmail.com>
2022-09-02 07:44:33 +0530

    push code

---
+++ server/index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app =express()
+const mysql =require('mysql')
+
+const db = mysql.createPool({
+    host:'localhost',
+    user:'root',
+    password:'Pazzw0rd@',
+    database:'inventorydb'
+})
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get('/api/get',(req,res)=>{
+    const sqlSELECT="SELECT * FROM inventorydb.serverinventory;";
+    db.query(sqlSELECT,(err,result)=>{
+        console.log(err)
+        console.log(result)
+        res.send(result)
+    })
+})
+app.put('/api/getBySearch/:id',(req,res)=>{
+    const sqlSELECT="SELECT * FROM inventorydb.serverinventory where ID=?;";
+    db.query(sqlSELECT,(err,result)=>{
+        res.send(result)
+    })
+})
+
+app.post('/api/insert',(req,res)=>{
+
+    const movieName = req.body.movieName
+    const movieReview = req.body.movieReview
+    const sqlInsert = "INSERT INTO inventorydb.serverinventory(movieName,movieReview) VALUES (?,?);"
+    db.query(sqlInsert,[movieName,movieReview],(err,result)=>{
+        console.log(result)
+    })
+})
+
+app.listen(3001,()=>{
+    console.log('running on port 3001')
+})
